fc1d37688ee604d4db2a43e00db5d91d6050c2fa
Angel luis <asanzv@iesch.org>
2022-12-05 20:43:22 +0100

    percepcion

--- lib/src/feature/identification/presentation/identification_screen.dart
+++ lib/src/feature/identification/presentation/identification_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:irobot/src/common_widgets/responsive_center.dart';
+import 'package:irobot/src/constants/app_sizes.dart';
 import 'package:irobot/src/localization/app_localizations_context.dart';
+import 'package:irobot/src/localization/string.hardcode.dart';
+import 'package:syncfusion_flutter_sliders/sliders.dart';
 
 class IdentificationScreen extends StatelessWidget {
   const IdentificationScreen({super.key});
@@ -9,6 +13,62 @@ class IdentificationScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 123, 201, 146),
       appBar: AppBar(title: Text(context.loc.identi_percep.toUpperCase())),
+      body: ResponsiveCenter(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  children: [
+                    Text('Estado del chip'.hardcoded),
+                    gapW8,
+                    const Icon(Icons.check)
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text('Alarma automatica'.hardcoded),
+                    gapW8,
+                    const Icon(Icons.check)
+                  ],
+                ),
+                Text('Sonido de la alarma'.hardcoded),
+                SfSlider(
+                  min: 0.0,
+                  max: 100.0,
+                  value: 60,
+                  showDividers: true,
+                  // shouldAlwaysShowTooltip: true,
+                  tooltipShape: const SfPaddleTooltipShape(),
+                  thumbIcon: const Center(child: Text('60')),
+                  showTicks: true,
+                  showLabels: true,
+                  minorTicksPerInterval: 1,
+                  onChanged: (dynamic value) {},
+                ),
+                gapH16,
+                Row(
+                  children: [
+                    Text('Señal encontrada'.hardcoded),
+                    gapW8,
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(),
+                        // borderRadius:
+                        //     const BorderRadius.all(Radius.circular(50)),
+                      ),
+                      child: const Text('1'),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
