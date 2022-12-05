413966c4da82eaaf2d5f38b76dc79949fd2d3fb1
Angel luis <asanzv@iesch.org>
2022-12-05 21:05:59 +0100

    regulate

--- lib/src/feature/regulate/presentation/regulate_screen.dart
+++ lib/src/feature/regulate/presentation/regulate_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:irobot/src/common_widgets/responsive_center.dart';
+import 'package:irobot/src/constants/app_sizes.dart';
 import 'package:irobot/src/localization/app_localizations_context.dart';
+import 'package:irobot/src/localization/string.hardcode.dart';
 
 class RegulateScreen extends StatelessWidget {
   const RegulateScreen({super.key});
@@ -9,6 +12,25 @@ class RegulateScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 123, 201, 146),
       appBar: AppBar(title: Text(context.loc.regula_modify.toUpperCase())),
+      body: ResponsiveCenter(
+        padding: const EdgeInsets.all(Sizes.p16),
+        child: Column(
+          children: [
+            Card(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text('Solicitud de video'.hardcoded),
+                  gapH16,
+                  const Text('Estrategias afsdasdkasl'),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
