8967398cd2ef7fe41dcd92189bf57db61cbc8f07
Angel luis <asanzv@iesch.org>
2022-12-14 15:39:32 +0100

    Video

--- lib/src/feature/regulate/presentation/regulate_screen.dart
+++ lib/src/feature/regulate/presentation/regulate_screen.dart
@@ -3,6 +3,7 @@ import 'package:irobot/src/common_widgets/responsive_center.dart';
 import 'package:irobot/src/constants/app_sizes.dart';
 import 'package:irobot/src/localization/app_localizations_context.dart';
 import 'package:irobot/src/localization/string.hardcode.dart';
+import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
 class RegulateScreen extends StatelessWidget {
   const RegulateScreen({super.key});
@@ -25,6 +26,7 @@ class RegulateScreen extends StatelessWidget {
                   Text('Solicitud de video'.hardcoded),
                   gapH16,
                   const Text('Estrategias afsdasdkasl'),
+                  const Youtube()
                 ],
               ),
             ),
@@ -32,5 +34,51 @@ class RegulateScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class Youtube extends StatefulWidget {
+  const Youtube({super.key});
+
+  @override
+  State<Youtube> createState() => _YoutubeState();
+}
+
+class _YoutubeState extends State<Youtube> {
+  late YoutubePlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = YoutubePlayerController(
+      params: const YoutubePlayerParams(
+        showControls: true,
+        mute: false,
+        showFullscreenButton: true,
+        loop: false,
+      ),
+    )
+      ..onInit = () {
+        _controller.loadVideoById(videoId: 'eu-2iWv_fCM');
+      }
+      ..onFullscreenChange = (isFullScreen) {
+        print('${isFullScreen ? 'Entered' : 'Exited'} Fullscreen.');
+      };
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return YoutubePlayerScaffold(
+      controller: _controller,
+      builder: (context, player) => Container(
+        child: player,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.close();
+    super.dispose();
   }
 }
